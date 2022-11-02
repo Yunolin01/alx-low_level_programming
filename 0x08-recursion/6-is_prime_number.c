0f5fb5f014b9269353d56eb6b53ecb4903a50c63
Yunolin <yunolinpillay@gmail.com>
2022-11-02 14:31:30 -0700

    task6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -26,3 +26,17 @@ int is_divisible(int num, int div)
 
 	return (is_divisible(num, div + 1));
 }
+
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: Number Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (prime2(n, 2));
+}
