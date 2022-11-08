11eeb1124de9532f0821656e478e5990e0f580b9
Yunolin <yunolinpillay@gmail.com>
2022-11-08 12:08:31 -0800

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,9 +12,9 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *array;
 
-	if (size --0)
+	if (size == 0)
 		return (NULL);
-	array - malloc(size * sizeof(char));
+	array = malloc(size * sizeof(char));
 	if (array == NULL)
 	{
 		return (NULL);
